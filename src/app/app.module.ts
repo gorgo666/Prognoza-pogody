c587cb29c48c79106d2f5113df8db78544a8b973
gorgo666 <grzegorz.wiecek.sluzbowy@gmail.com>
2020-05-12 09:37:51 +0200

    delete not needed components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
-import { GalleryComponent } from './gallery/gallery.component';
 import { WeatherComponent } from './weather/weather.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HelloComponent,
-    GalleryComponent,
-    WeatherComponent,
-  ],
+  declarations: [AppComponent, WeatherComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
